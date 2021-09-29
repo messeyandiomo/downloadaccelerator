df6cd295446e9465cde0e23f73b0cb6ca4368841
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2021-09-29 20:39:31 +0100

    Update downloadaccelerator.sh

--- downloadaccelerator.sh
+++ downloadaccelerator.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-export JAVA_HOME=$SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH
-export PATH=$SNAP/bin:$SNAP/sbin:$SNAP/usr/bin:$SNAP/usr/sbin:$JAVA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$SNAP/lib:$SNAP/lib/$SNAP_ARCH_TRIPLET:$SNAP/usr/lib:$SNAP/usr/lib/$SNAP_ARCH_TRIPLET:$SNAP_LIBRARY_PATH:$LD_LIBRARY_PATH
-
-DACAUTOSTART=$HOME/.config/autostart
-DACAUTOSTARTDESKTOP=$SNAP_NAME-server.desktop
-DACJARPATHNAME=$SNAP/$SNAP_NAME-$SNAP_VERSION.jar
-
-
-###creation of autostart desktop file
-if [ ! -e $DACAUTOSTART ]
-then
-	mkdir -p $DACAUTOSTART
-fi
-
-cd $SNAP
-
-if [ ! -e $DACAUTOSTART/$DACAUTOSTARTDESKTOP ]
-then
-	cp $DACAUTOSTARTDESKTOP $DACAUTOSTART
-	setsid java -jar $DACJARPATHNAME server &
-fi
-
-###execution of the command
-if [ -z $1 ]
-then
-	java -jar $DACJARPATHNAME client
-else
-	java -jar $DACJARPATHNAME server
-fi
+if [ ! -d "$SNAP_USER_DATA/.config/autostart" ];
+  then
+    mkdir -p $SNAP_USER_DATA/.config/autostart
+    ln -sfnt $SNAP_USER_DATA/.config/autostart/ $SNAP/autostart/$SNAP_NAME-server.desktop
+  fi
