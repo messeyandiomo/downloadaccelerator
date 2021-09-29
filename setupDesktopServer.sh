ebd70d1b78725762ceb4fc9b2332b2f940cb2ff1
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2021-09-29 21:03:45 +0100

    Update setupDesktopServer.sh

--- setupDesktopServer.sh
+++ setupDesktopServer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ ! -d "$SNAP_USER_DATA/.config/autostart" ];
+if [ ! -e $SNAP_USER_DATA/.config/autostart/$SNAP_NAME-server.desktop ]
+then
+  if [ ! -d $SNAP_USER_DATA/.config/autostart ]
   then
     mkdir -p $SNAP_USER_DATA/.config/autostart
     ln -sfnt $SNAP_USER_DATA/.config/autostart/ $SNAP/autostart/$SNAP_NAME-server.desktop
   fi
+fi
