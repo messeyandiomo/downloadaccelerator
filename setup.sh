85d2e4c2251f35d21fc594762a408a5f9e34c9fc
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2021-09-30 06:54:25 +0100

    Update and rename downloadaccelerator.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export JAVA_BIN = $SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH/bin/java
+
+
 if [ ! -e $SNAP_USER_DATA/.config/autostart/$SNAP_NAME-server.desktop ]
 then
   if [ ! -d $SNAP_USER_DATA/.config/autostart ]
@@ -7,13 +10,12 @@ then
     mkdir -p $SNAP_USER_DATA/.config/autostart
   fi
   ln -sfnt $SNAP_USER_DATA/.config/autostart/ $SNAP/autostart/$SNAP_NAME-server.desktop
+  setsid $JAVA_BIN -jar $SNAP/$SNAP_NAME-$SNAP_VERSION.jar  server &
 fi
 
 cd $SNAP
 
 if [ ! -z $1 ]
 then
-  desktop-launch $JAVA_BIN -jar $SNAP/$SNAPCRAFT_PROJECT_NAME-$SNAPCRAFT_PROJECT_VERSION.jar  client
-else
-  desktop-launch $JAVA_BIN -jar $SNAP/$SNAPCRAFT_PROJECT_NAME-$SNAPCRAFT_PROJECT_VERSION.jar  server
+  $JAVA_BIN -jar $SNAP/$SNAP_NAME-$SNAP_VERSION.jar  client
 fi
