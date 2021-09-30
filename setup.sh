00080af360d886ee397e03bd3fea2aa61a0c502c
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2021-09-30 08:46:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export JAVA_BIN = $SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH/bin/java
-
-
 if [ ! -e $SNAP_USER_DATA/.config/autostart/$SNAP_NAME-server.desktop ]
 then
   if [ ! -d $SNAP_USER_DATA/.config/autostart ]
@@ -10,7 +7,7 @@ then
     mkdir -p $SNAP_USER_DATA/.config/autostart
   fi
   ln -sfnt $SNAP_USER_DATA/.config/autostart/ $SNAP/autostart/$SNAP_NAME-server.desktop
-  setsid $JAVA_BIN -jar $SNAP/$SNAP_NAME-$SNAP_VERSION.jar  server &
+  $JAVA_BIN -jar $SNAP/$SNAP_NAME-$SNAP_VERSION.jar  server &
 fi
 
 cd $SNAP
