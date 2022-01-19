3e194875acc251f2c1fdc5ba9736c6f8538a8066
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2022-01-19 15:31:03 +0100

    Add files via upload

---
+++ src/DownloadDirectories.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DownloadDirectories {
+	
+	private String destinationDirectory = "";
+	private String tempDirectory = "";
+	private String cacheDirectory = "cacheofdac";
+	private String ffmpegPathName = "ffmpeg";
+	private static String imagesDirectory = "images" + System.getProperty("file.separator");
+	private static String dacLogo = imagesDirectory + "daclogo.png";
+
+	public DownloadDirectories() {
+		// TODO Auto-generated constructor stub
+		if(System.getProperty("os.name").matches("Linux")) {
+        	this.setDestinationDirectory(System.getenv("HOME"));
+        	this.setCacheDirectory(this.getDestinationDirectory() + "." + cacheDirectory);
+        	this.setTempDirectory("/tmp");
+        	String nameRe = "[a-zA-Z_$][a-zA-Z_$0-9]*";
+        	Pattern snapPattern = Pattern.compile(String.format("^\\/home\\/(?:%s)\\/snap\\/(?<snapname>%s)\\/(?<snaprevision>[a-zA-Z_$0-9.]+)(?:\\/)?$", nameRe, nameRe));
+        	Matcher snapMatcher = snapPattern.matcher(this.getDestinationDirectory());
+        	if(snapMatcher.find()) {
+        		String snapname = snapMatcher.group("snapname");
+        		String snaprevision = snapMatcher.group("snaprevision");
+        		this.setFfmpegPathName("/snap/" + snapname + "/" + snaprevision + "/usr/bin/" + this.getFfmpegPathName());
+        	}
+        }
+        else if(System.getProperty("os.name").matches("Windows")){
+        	this.setDestinationDirectory(System.getenv("%HOMEPATH%"));
+        	this.setCacheDirectory(this.getDestinationDirectory() + cacheDirectory);
+        	this.setTempDirectory(System.getenv("%TEMP%"));
+        }
+		this.createCacheDirectory();
+	}
+	
+	
+	private synchronized void createCacheDirectory() {
+		
+		File theDir = new File(this.getCacheDirectory());
+		if (!theDir.exists())
+		    theDir.mkdirs();
+	}
+	
+	public static String getImagesDirectory() {
+		return imagesDirectory;
+	}
+	
+	
+	public String getDestinationDirectory() {
+		return destinationDirectory;
+	}
+
+
+	public void setDestinationDirectory(String destinationdirectory) {
+		this.destinationDirectory = destinationdirectory + System.getProperty("file.separator");
+	}
+
+
+	public String getTempDirectory() {
+		return tempDirectory;
+	}
+
+
+	public void setTempDirectory(String tempdirectory) {
+		this.tempDirectory = tempdirectory + System.getProperty("file.separator");
+	}
+
+
+	public static String getPathLogo() {
+		return dacLogo;
+	}
+
+
+	public String getCacheDirectory() {
+		return cacheDirectory;
+	}
+	
+	
+	private void setCacheDirectory(String cachedir) {
+		this.cacheDirectory = cachedir + System.getProperty("file.separator");
+	}
+
+
+	public String getFfmpegPathName() {
+		return ffmpegPathName;
+	}
+
+
+	private void setFfmpegPathName(String ffmpegPathName) {
+		this.ffmpegPathName = ffmpegPathName;
+	}
+
+
+	/*public void setPathLogo(String pathlogo) {
+		dacLogo = pathlogo;
+	}*/
+
+}
