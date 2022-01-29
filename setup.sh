b5b35e0cb77388289ac5ad2c364b2a5e825cc8c9
messey-bilal <63718240+messey-bilal@users.noreply.github.com>
2022-01-29 08:44:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ then
     mkdir -p $SNAP_USER_DATA/.config/autostart
   fi
   ln -sfnt $SNAP_USER_DATA/.config/autostart/ $SNAP/autostart/$SNAP_NAME-server.desktop
+  export PATH=$PATH:$SNAP/bin:$SNAP/sbin:$SNAP/usr/bin:$SNAP/usr/sbin:$JAVA_HOME/bin:
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SNAP_LIBRARY_PATH:$SNAP/lib:$SNAP/lib/$SNAPCRAFT_ARCH_TRIPLET:$SNAP/usr/lib:$SNAP/usr/lib/$SNAPCRAFT_ARCH_TRIPLET
   $JAVA_BIN -jar $SNAP/$SNAP_NAME-$SNAP_VERSION.jar  server &
 fi
 
