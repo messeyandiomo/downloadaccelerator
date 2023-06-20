a9f1e427502463ade2ba700ca0fd5f555e15c671
Messey à Ndiomo <messey.bilal@gmail.com>
2023-06-20 12:09:34 +0100

    release 3.2: adding of modules DownloadProps, SubDownloadProps, SubDownloadPropsFactoriesManager, SubDownloadPropsFactory

---
+++ src/SubDownloadPropsFactoriesManager.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+
+public class SubDownloadPropsFactoriesManager extends Thread implements Observable {
+	
+	private int subDownloadPropsGenerated = 0;
+	private DownloadProps downloadProps = null;
+	private DownloadProps audioDownloadProps = null;
+	private ArrayList<Observer> listObserver = new ArrayList<Observer>();
+	private int subDownloadCount = 0;
+	
+	
+	/*** newer gererators ***/
+	public SubDownloadPropsFactoriesManager(DownloadProps downloadprops) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.setDownloadProps(downloadprops);
+		this.subDownloadCount = downloadProps.getSubDownloadCount();
+		this.start();
+	}
+	
+	public SubDownloadPropsFactoriesManager(DownloadProps downloadprops, DownloadProps audiodownloadprops) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.setDownloadProps(downloadprops);
+		this.setAudioDownloadProps(audiodownloadprops);
+		this.subDownloadCount = downloadProps.getSubDownloadCount() + audioDownloadProps.getSubDownloadCount();
+		this.start();
+	}
+	
+	
+	public void run() {
+		
+		int numberofsubdownloadpropsgenerated = 0;
+		while(true) {
+			synchronized (this) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			numberofsubdownloadpropsgenerated = this.getSubDownloadPropsGenerated();
+			if(numberofsubdownloadpropsgenerated == (this.subDownloadCount)) {
+				this.updateObserver();
+				break;
+			}
+		}
+	}
+
+	public DownloadProps getDownloadProps() {
+		return downloadProps;
+	}
+
+	public void setDownloadProps(DownloadProps downloadprops) {
+		this.downloadProps = downloadprops;
+	}
+	
+	public DownloadProps getAudioDownloadProps() {
+		return audioDownloadProps;
+	}
+
+	public void setAudioDownloadProps(DownloadProps audiodownloadprops) {
+		this.audioDownloadProps = audiodownloadprops;
+	}
+
+	public synchronized int getSubDownloadPropsGenerated() {
+		return subDownloadPropsGenerated;
+	}
+
+	public synchronized void setSubDownloadPropsGenerated() {
+		this.subDownloadPropsGenerated++;
+		notify();
+	}
+	
+	
+	public void recordSubDownloadProps(int subdownloadpropsnumber, SubDownloadProps subdownloadprops) {
+		if(subdownloadprops.isAudio())
+			this.audioDownloadProps.setSubDownloadProps(subdownloadpropsnumber, subdownloadprops);
+		else if(subdownloadprops.isVideo())
+			this.downloadProps.setSubDownloadProps(subdownloadpropsnumber, subdownloadprops);
+		this.setSubDownloadPropsGenerated();
+	}
+	
+
+
+	@Override
+	public void addObserver(Observer obs) {
+		// TODO Auto-generated method stub
+		this.listObserver.add(obs);
+	}
+
+
+	@Override
+	public void updateObserver() {
+		// TODO Auto-generated method stub
+		for(Observer obs : this.listObserver)
+			obs.update(true, 0);
+	}
+
+
+	@Override
+	public void delObserver() {
+		// TODO Auto-generated method stub
+		this.listObserver = new ArrayList<Observer>();
+	}
+
+}
