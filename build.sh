39ebcfa00186b2dcc8ef73ac02b351a9ed49a45c
messey à ndiomo <messey.bilal@gmail.com>
2023-12-11 07:36:55 +0100

    updating of build.sh script

--- build.sh
+++ build.sh
@@ -3,4 +3,17 @@
 PROJECT_NAME="downloadaccelerator"
 VERSION="3.2"
 
-mkdir out && javac -d out -classpath libs/*:.  -sourcepath src/ src/utils/DaServer.java && cp libs/*.jar out/ && cp MANIFEST.MF out/ && cd out && jar cfm $PROJECT_NAME-$VERSION.jar  MANIFEST.MF . && cp $PROJECT_NAME-$VERSION.jar ../ && cd .. && rm -rf out
+## create a temporary folder named out
+mkdir out && 
+## compile the app from src folder
+javac -d out -classpath libs/*:.  -sourcepath src/ src/utils/DaServer.java && 
+## move jar dependencies and go to out dir
+cp libs/*.jar out/ && cd out && 
+## extract jar dependencies files
+find ./ -name "*.jar" -exec jar -xf {} \; && 
+## delete jar files
+rm -f *.jar && 
+## create the app jar
+jar cfe $PROJECT_NAME-$VERSION.jar utils.DaServer . && 
+## move jar app to the parent dir and change to the parent dir then delete temporary folder 
+cp $PROJECT_NAME-$VERSION.jar ../ && cd .. && rm -rf out
